cf7dba2787c59534b3fb3f678f30fb912b07f454
EthanS <ethanski22@icloud.com>
2020-02-26 19:15:19 -0500

    a

---
+++ Week1/New folder/CS101Review/no/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace no
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void barkbutton_Click(object sender, RoutedEventArgs e)
+        {
+            string dogname = namebox.Text;
+            dog dog = new dog();
+            dog.name = dogname;
+            dog.sayname();
+        }
+
+        private void buttonbark_Click(object sender, RoutedEventArgs e)
+        {
+            dog dog = new dog();
+            dog.bark();
+        }
+    }
+}
---
+++ Week1/New folder/CS101Review/no/dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace no
+{
+    class dog
+    {
+        public string breed;
+        public string name;
+        public int height;
+        
+        public void bark()
+        {
+            MessageBox.Show("bark");
+        }
+        public void sayname()
+        {
+            MessageBox.Show("my name is " + name);
+        }
+
+    }
+}
